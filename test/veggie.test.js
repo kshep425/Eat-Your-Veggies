81b5bcd57cadf5d67b1d4fed4fa47aac1c7a78af
kshep425 <9432996+kshep425@users.noreply.github.com>
2020-01-23 00:40:51 -0500

    Trying to figure out how to remove all of the test veggies before and after all tests run

--- test/veggie.test.js
+++ test/veggie.test.js
@@ -2,23 +2,20 @@ const Veggie = require("../Veggie.js");
 const connection = require("../config/db_connection")
 const orm = require("../config/orm")
 const util = require("util")
-const orm_remove_test_veggies = util.promisify(orm.remove_test_veggies)
+// const orm_remove_test_veggies = util.promisify(orm.remove_test_veggies)
 
 describe("Veggie Class Tests", () => {
     beforeAll(() => {
-        orm_remove_test_veggies()
-        .then((result) => {return result})
-        .catch((err) => {throw err});
+        // orm.remove_test_veggies(function(cb){
+        //     cb("Removed Test Veggies");
+        // })
     });
 
     afterAll(() => {
-        orm_remove_test_veggies()
-        .then((result) => {
+        //orm.remove_test_veggies(function(){
             connection.end();
-            return result
-        })
-        .catch((err) => {throw err});
-    });
+        //})
+     });
 
     it("Can Instantiate a Veggie", () => {
         const veg = new Veggie();
@@ -28,18 +25,17 @@ describe("Veggie Class Tests", () => {
     it("Can add Veggie", () => {
         const veg = new Veggie();
         const veg_name = "Test_Spinach"
-        veg.add(veg_name, function(result){
+        veg.add(veg_name, function(){
             expect(veg.get_veg_name()).toEqual(veg_name)
             expect(veg.get_veg_state()).toBe(false);
-
         });
     })
 
     it("Can devour a Veggie", () => {
         const veg = new Veggie();
         const veg_name = "Test_Radish"
-        veg.add(veg_name, function(result){
-            veg.devour(function(result){
+        veg.add(veg_name, function(){
+            veg.devour(function(){
                 expect(veg.get_veg_name()).toEqual(veg_name)
                 expect(veg.get_veg_state()).toBe(true);
             })
@@ -61,16 +57,34 @@ describe("Veggie Class Tests", () => {
         }).toThrow("Error: Please include name of vegetable");
     })
 
-    it ("Can get new veg information", () => {
+    it ("Can add veggie to db", () => {
         const veg = new Veggie();
         const veg_name = "Test_Radish"
-        const result = veg.add(veg_name, function(result){
-            const expected = {
-                veg_id: result.insertId,
-                veg_name: veg_name,
-                veg_state: false
-            }
-            expect(veg.get_veg()).toEqual(expected)
+        veg.add(veg_name, function (db_result) {
+
+                const expected = {
+                    veg_id: db_result.insertId,
+                    veg_name: veg_name,
+                    veg_state: false
+                }
+                expect(veg.get_veg()).toEqual(expected)
+
+        })
+    })
+
+    it ("Can add a devoured veggie to db", () => {
+        const veg = new Veggie();
+        const veg_name = "Test_Radish"
+        veg.add(veg_name, function (db_result) {
+            veg.devour(function(){
+                    const expected = {
+                        veg_id: db_result.insertId,
+                        veg_name: veg_name,
+                        veg_state: true
+                    }
+                    expect(veg.get_veg()).toEqual(expected)
+
+            })
         })
     })
 
@@ -78,14 +92,16 @@ describe("Veggie Class Tests", () => {
         const veg = new Veggie();
         const veg_name = "Test_Cabbage"
         veg.add(veg_name, function(add_result){
-            veg.devour(function(){
-                const expected = {
-                    veg_id: add_result.insertId,
-                    veg_name: veg_name,
-                    veg_state: true
-                }
-                expect(veg.get_veg()).toEqual(expected)
-            });
+                veg.devour(function(){
+
+                    const expected = {
+                        veg_id: add_result.insertId,
+                        veg_name: veg_name,
+                        veg_state: true
+                    }
+                    expect(veg.get_veg()).toEqual(expected)
+
+            })
 
         });
     })

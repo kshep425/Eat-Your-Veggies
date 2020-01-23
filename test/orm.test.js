47f9f01fd0bc3a06800e868bbdf8a0b9921ab483
kshep425 <9432996+kshep425@users.noreply.github.com>
2020-01-23 00:39:10 -0500

    Added tests for a few functions, figured out issue where test would pass even though it threw an error by adding a callback to the top level function

--- test/orm.test.js
+++ test/orm.test.js
@@ -2,35 +2,63 @@ const orm = require("../config/orm");
 const connection = require("../config/db_connection");
 
 describe("ORM Tests",()=> {
+
     afterAll(() => {
-        //orm_remove_test_veggies()
-        //.then((result) => {
-            connection.end();
-            // return result;
-        //})
-        //.catch((err) => {throw err});
+        connection.end();
     });
-    it("Can select all veg_names with veg_state of true", ()=>{
-        connection.query("SELECT veg_name FROM veggies WHERE veg_state=TRUE", function(err, res){
 
-            expect(() =>{
-                orm.select_devoured(function(result){
-                    expect(res).toEqual(false);
-                })
-            });
-        })
-    })
+    it ("Can select all veg_names with veg_state of true", function(cb) {
+        orm.select_devoured(function(result){
+            const query_string =
+            `
+                SELECT veg_name
+                FROM veggies
+                WHERE veg_state=TRUE
+            `;
 
-    it("Can select all veg_names with veg_state of false", ()=>{
-        connection.query("SELECT veg_name FROM veggies WHERE veg_state=FALSE", function(err, res){
-//            expect(err).toBe(false);
-            expect(()=>{
-                orm.select_not_eaten(function(result){
-                    expect(res).toEqual(false);
-                });
+            connection.query(query_string, function(err, conn_result){
+                cb(expect(result).toEqual(conn_result))
             })
-
         })
-
     })
+
+    it ("Can select all veg_names with veg_state of false", function(cb) {
+        orm.select_not_eaten(function(result){
+            const query_string =
+            `
+                SELECT veg_name
+                FROM veggies
+                WHERE veg_state=FALSE
+            `;
+
+            connection.query(query_string, function(err, conn_result){
+                cb(expect(result).toEqual(conn_result))
+            })
+        })
+    })
+
+    it ("Can remove test veggies", function(cb){
+        orm.remove_test_veggies(function(result){
+            const query_string =
+            `
+                SELECT count(*) as count
+                FROM veggies
+                WHERE veg_name LIKE "%Test%"
+            `;
+
+            connection.query(query_string, function(err, conn_result){
+                cb(expect(conn_result[0]["count"]).toEqual(0))
+            })
+        })
+    })
+
+    // it("add_veggie should callback", function(){
+    //     const mock_callback = jest.fn((x) => {
+    //         // console.log(x)
+    //         return x});
+    //     orm.add_veggie("Test_Collard_Greens", false, mock_callback);
+    //     //expect(mock_callback.mock.calls.length).toBe(1);
+    //     expect(mock_callback.mock.result).toBe(1);
+    // })
+
 })

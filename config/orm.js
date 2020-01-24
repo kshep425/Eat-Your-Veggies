4c67775c5a93df2abf2c82d0c6e9e40affd5e6c3
kshep425 <9432996+kshep425@users.noreply.github.com>
2020-01-24 16:29:34 -0500

    updated orm to be an oject that included connection for initialize_connection object

--- config/orm.js
+++ config/orm.js
@@ -1,9 +1,21 @@
-const connection = require("./db_connection")
+const initialize_connection = require("./db_connection")
+const db_conn_obj =
+{
+    database: "veggies_db",
+    user: "root",
+    password: "yourRootPassword",
+    host: "localhost",
+    port: 3306
+}
 require("console.table")
 
 const orm = {
+    connection: initialize_connection(db_conn_obj),
+
     add_veggie: function (veg_name, veg_state, callback) {
-        if(veg_name === undefined){
+        // https://www.sitepoint.com/javascript-truthy-falsy/
+        // runs if veg_name is false, 0, '', NaN, null or undefined
+        if(!veg_name){
             throw ("Please include name of vegetable")
         }
         const query_string =
@@ -11,27 +23,30 @@ const orm = {
                 INSERT INTO veggies(veg_name, veg_state)
                 VALUES (?, ?)
             `
-        connection.query(query_string, [veg_name, veg_state], function (err, res) {
-            if (err) throw ("orm.add_veggie err: ", err);
+        this.connection.query(query_string, [veg_name, veg_state], function (err, res) {
+            if (err) throw ("Failed to add Veggie");
             // console.log(res)
             //console.log(res.insertId)
             callback(res)
         })
     },
 
-    // devour_veggie: function (veg_name, callback) {
-    //     const query_string =
-    //         `
-    //             UPDATE veggies
-    //             SET veg_state = true
-    //             WHERE veg_name = ?
-    //         `;
-    //     connection.query(query_string, [veg_name], function (err, res) {
-    //         if (err) throw err;
-    //         // console.log(res)
-    //         callback(res);
-    //     })
-    // },
+    update_veg_state: function (veg_id, veg_state, callback) {
+        if(veg_id === undefined){
+            throw ("Please include vegetable id")
+        }
+        const query_string =
+            `
+                UPDATE veggies
+                SET veg_state = ?
+                WHERE veg_id = ?
+            `;
+        this.connection.query(query_string, [veg_state, veg_id], function (err, res) {
+            if (err) throw err;
+            // console.log(res)
+            callback(res);
+        })
+    },
 
     remove_test_veggies: function (callback) {
         const query_string =
@@ -41,7 +56,7 @@ const orm = {
             WHERE veg_name LIKE "%Test%"
         `;
 
-        connection.query(query_string, function(err, res){
+        this.connection.query(query_string, function(err, res){
             if (err) throw err;
             // console.table(res);
             callback(res);
@@ -56,7 +71,7 @@ const orm = {
             WHERE veg_state=TRUE
         `;
 
-        connection.query(query_string, function(err, res){
+        this.connection.query(query_string, function(err, res){
             if (err) throw err;
             // console.table(res);
             callback(res);
@@ -71,7 +86,7 @@ const orm = {
             WHERE veg_state=FALSE
         `;
 
-        connection.query(query_string, function(err, res){
+        this.connection.query(query_string, function(err, res){
             if (err) throw err;
             // console.table(res);
             callback(res);

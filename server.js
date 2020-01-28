c56fea7ddaa15b3899439c495b4f2b2c425e4eae
kshep425 <9432996+kshep425@users.noreply.github.com>
2020-01-28 00:06:07 -0500

    Added scripts rout for nprogress

--- server.js
+++ server.js
@@ -52,11 +52,13 @@ const index_routes = require("./routes/index")
 app.use(api_routes);
 app.use(index_routes);
 
+// Use Scripts route to have a static accessible node_modules folder
+app.use('/scripts', express.static(__dirname + '/node_modules/nprogress/'));
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
   // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+  console.log("Server listening on port:" + PORT);
 });
 
 
